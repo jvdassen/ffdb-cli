07d383d5abd9011cedf2a0070532388585f8bd9b
Jan von der Assen <jan.vda@gmx.ch>
2019-03-21 11:57:31 +0100

     Define data structures

---
+++ cli.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+type macroNutrientProfile struct {
+	protein       string
+	carbohydrates string
+	fats          string
+}
+
+type hyperLink struct {
+	text string
+	url  string
+}
+
+func main() {
+
+}
